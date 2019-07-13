f80b72308c5007757ffdc797d12c5f7dfcb81851
Semyon Minyushov <minyushov@gmail.com>
2019-07-13 21:24:41 +0400

    ViewHolder should be open

--- androidx-recyclerview-ktx/src/main/java/androidxx/recyclerview/widget/ViewHolder.kt
+++ androidx-recyclerview-ktx/src/main/java/androidxx/recyclerview/widget/ViewHolder.kt
@@ -3,4 +3,4 @@ package androidxx.recyclerview.widget
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ViewHolder<T : View>(val view: T) : RecyclerView.ViewHolder(view)
+open class ViewHolder<T : View>(val view: T) : RecyclerView.ViewHolder(view)
